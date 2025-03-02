b65ce0743d41fbfa7a5cc3ce1ee50d519779dcea
timger <yishenggudou@gmail.com>
2025-03-02 11:45:01 +0800

    add main

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import './styles.css';
+import {LoadToThinkModal} from './views/LoadToThinkModal';
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
@@ -17,17 +18,19 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('Foldspace', 'Foldspace助手', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass('foldspace-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
+		statusBarItemEl.setText('Foldspace助手');
+		statusBarItemEl.addEventListener('click', () => {
+			new LoadToThinkModal(this.app).open();
+		});
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
