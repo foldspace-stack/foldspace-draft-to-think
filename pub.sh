8f317924fa652ede4c16cd72b646f6df75e50119
timger <yishenggudou@gmail.com>
2025-04-10 17:42:06 +0800

    add

---
+++ pub.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -vrf ./*   ../block-cutter/block-cutter-docs/.obsidian/plugins/foldspace-draft-to-think/
+cp -vrf ./*   ../foldspace-stack.github.io/.obsidian/plugins/foldspace-draft-to-think/
+#cp -vrf ./*  /Volumes/dafengstudio/自媒体视频素材/素材洗稿/0000/.obsidian/plugins/foldspace-draft-to-think
