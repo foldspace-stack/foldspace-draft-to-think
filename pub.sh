d3652dbf6774e9de66b3e3b69db49d0921dd3526
timger <yishenggudou@gmail.com>
2025-07-19 14:24:30 +0800

    add

--- pub.sh
+++ pub.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-cp -vrf ./main*   ../block-cutter/block-cutter-docs/.obsidian/plugins/foldspace-draft-to-think/
-cp -vrf ./*.json   ../block-cutter/block-cutter-docs/.obsidian/plugins/foldspace-draft-to-think/
-cp -vrf ./*.css   ../block-cutter/block-cutter-docs/.obsidian/plugins/foldspace-draft-to-think/
-cp -vrf ./main*    ../foldspace-stack.github.io/.obsidian/plugins/foldspace-draft-to-think/
-cp -vrf ./*.json    ../foldspace-stack.github.io/.obsidian/plugins/foldspace-draft-to-think/
-cp -vrf ./*.css    ../foldspace-stack.github.io/.obsidian/plugins/foldspace-draft-to-think/
+#!/bin/bash
+
+directories=("../block-cutter/block-cutter-docs/.obsidian/plugins/foldspace-draft-to-think/" "/Volumes/dafengstudio/自媒体视频素材/素材洗稿/0000/.obsidian/plugins/foldspace-draft-to-think/")
+
+
+
+for dir in "${directories[@]}"; do
+    if [ -d "$dir" ]; then
+        echo "存在: $dir copy 插件"
+        cp -vrf ./main*   ${$dir}
+        cp -vrf ./*.json   ${$dir}
+        cp -vrf ./*.css   ${$dir}
+        cp -vrf ./main*    ${$dir}
+        cp -vrf ./*.json    ${$dir}
+        cp -vrf ./*.css    ${$dir}
+    else
+        echo "不存在: $dir"
+    fi
+done
+
+echo "========================="
+
 #cp -vrf ./*  /Volumes/dafengstudio/自媒体视频素材/素材洗稿/0000/.obsidian/plugins/foldspace-draft-to-think
