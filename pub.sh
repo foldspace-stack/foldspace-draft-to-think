016c0c2dce4d21095b5cee2febe2c7abfbac3c42
timger <yishenggudou@gmail.com>
2025-07-19 14:29:23 +0800

    add

--- pub.sh
+++ pub.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 
 directories=("../block-cutter/block-cutter-docs/.obsidian/plugins/foldspace-draft-to-think/" "/Volumes/dafengstudio/自媒体视频素材/素材洗稿/0000/.obsidian/plugins/foldspace-draft-to-think/")
 
@@ -8,12 +7,12 @@ directories=("../block-cutter/block-cutter-docs/.obsidian/plugins/foldspace-draf
 for dir in "${directories[@]}"; do
     if [ -d "$dir" ]; then
         echo "存在: $dir copy 插件"
-        cp -vrf ./main*   ${$dir}
-        cp -vrf ./*.json   ${$dir}
-        cp -vrf ./*.css   ${$dir}
-        cp -vrf ./main*    ${$dir}
-        cp -vrf ./*.json    ${$dir}
-        cp -vrf ./*.css    ${$dir}
+        cp -vrf ./main*   $dir
+        cp -vrf ./*.json   $dir
+        cp -vrf ./*.css   $dir
+        cp -vrf ./main*    $dir
+        cp -vrf ./*.json    $dir
+        cp -vrf ./*.css    $dir
     else
         echo "不存在: $dir"
     fi
